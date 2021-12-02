8611354d883d27590f4385527451b2c45265f92e
Thomas Martin <thomas@oopss.org>
2021-12-02 12:32:35 +0100

    Add 02b

---
+++ 02b.py
@@ -0,0 +1,21 @@
+ins = {
+    'forward': (1, 0),
+    'down': (0, 1),
+    'up': (0, -1),
+}
+moves = [
+    [x * int(s[1]) for x in ins[s[0]]]
+    for line
+    in open('02b_input').readlines()
+    if (s := line.strip().split(' '))
+]
+aim = forward = depth = 0
+for f, d in moves:
+    if f:
+        forward += f
+        if aim:
+            depth += f * aim
+    elif d:
+        aim += d
+print(forward, depth)
+print(forward * depth)
